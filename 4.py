20ba58dbcb0165a8c18df8a819c5d7d6289939c3
Bartosz Bryniarski <brylka@jelenia.com>
2025-12-06 12:56:09 +0100

    Sobota

---
+++ 4.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+
+digits = load_digits()
+X, y = digits.data, digits.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 5, 10],
+    'max_features': ['sqrt', 'log2']
+}
+
+
+
