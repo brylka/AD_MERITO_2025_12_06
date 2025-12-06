487ed9d60bc944bd389b2924a9a68762e2a00914
Bartosz Bryniarski <brylka@jelenia.com>
2025-12-06 10:58:41 +0100

    Sobota

--- 3.py
+++ 3.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
 
 
 digits = load_digits()
@@ -10,3 +11,11 @@ print(f"Liczba próbek: {X.shape[0]}")
 print(f"Liczba cech:   {X.shape[1]}")
 print(f"Liczba klas:   {len(np.unique(y))}")
 
+fig, axes = plt.subplots(2,5, figsize=(12,5))
+for i, ax in enumerate(axes.flat):
+    ax.imshow(digits.images[i], cmap='gray')
+    ax.set_title(f"Cyfra: {digits.target[i]}")
+    ax.axis('off')
+plt.suptitle('Przykładowe cyfry ze zbioru Digits', fontsize=14)
+plt.tight_layout()
+plt.show()
