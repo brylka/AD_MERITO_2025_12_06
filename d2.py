57c94b6576a99de6003ff9cddb88e899eb3eabaf
Bartosz Bryniarski <brylka@jelenia.com>
2025-12-07 12:25:57 +0100

    Niedziela

--- d2.py
+++ d2.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 import joblib
 
 model = joblib.load('model.joblib')
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        return "Odebrałem plik"
     return render_template('d2.html')
 
 if __name__ == '__main__':
