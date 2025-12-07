251e4001f015782a2c33053ca7e5339a7f76064b
Bartosz Bryniarski <brylka@jelenia.com>
2025-12-07 12:45:54 +0100

    Niedziela

--- d2.py
+++ d2.py
@@ -1,5 +1,6 @@
+import numpy as np
 from flask import Flask, render_template, request
-
+from PIL import Image
 import joblib
 
 model = joblib.load('model.joblib')
@@ -7,9 +8,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    digit = ''
     if request.method == 'POST':
-        return "Odebrałem plik"
-    return render_template('d2.html')
+        file = request.files['image']
+        img = Image.open(file).convert('L')
+        img = img.resize((8,8))
+        data = np.array(img)
+        data = 16 - (data / 255.0 * 16)
+        data = data.flatten().reshape(1,-1)
+        digit = int(model.predict(data)[0])
+
+    return render_template('d2.html', digit=digit)
 
 if __name__ == '__main__':
     app.run(debug=True)
