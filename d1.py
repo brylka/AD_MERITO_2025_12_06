cb56477b0cb44c294e8753b4c5c37662601d5061
Bartosz Bryniarski <brylka@jelenia.com>
2025-12-07 12:08:46 +0100

    Niedziela

--- d1.py
+++ d1.py
@@ -2,10 +2,14 @@ import joblib
 from sklearn.datasets import load_digits
 from sklearn.ensemble import RandomForestClassifier
 
+# wczytanie danych - digits
 digits = load_digits()
 X, y = digits.data, digits.target
 
+# tworzenie i trening modelu
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X, y)
 
+# zapisanie modelu do pliku
 joblib.dump(model, 'model.joblib')
+print("Zapisano model do pliku!")
